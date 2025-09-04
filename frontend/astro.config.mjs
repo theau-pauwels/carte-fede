6c905a798c32c2dd04264cf5a4cbc42afb9c3bd1
Theau Pauwels <83910999+theau-pauwels@users.noreply.github.com>
2025-09-04 23:31:07 +0200

    Add files via upload

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,5 +1,49 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://carte-fede-test.magellan.fpms.ac.be', // ← important pour URLs absolues
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+    robotsTxt(),
+  ],
+  site: "http://carte-fede-test.magellan.fpms.ac.be",
+  output: "static",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+  serverOptions: {
+    headers: {
+      "Content-Security-Policy":
+        "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self';",
+    },
+  },
+  vite: {
+     server: {
+       host: true,
+       port: 3000,
+       allowedHosts: [
+         'frontend',
+         'localhost',
+         '127.0.0.1',
+         '::1',
+         'magellan.fpms.ac.be',
+       ],
+       hmr: {
+         protocol: 'ws',
+         host: process.env.HMR_HOST || 'localhost',
+         port: 80,
+      },
+    },
+  },
 });
